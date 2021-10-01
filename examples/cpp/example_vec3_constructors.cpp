4009167fa86d5533340e687d85dbf14011bc7041
wpumacay <wpumacay@gmail.com>
2021-10-01 06:41:46 -0500

    [Add-feat]: to-string and stdin|stdout vec3 support

--- examples/cpp/example_vec3_constructors.cpp
+++ examples/cpp/example_vec3_constructors.cpp
@@ -5,8 +5,6 @@
 
 template <typename T>
 auto printVector(const tiny::math::Vector3<T>& vec) -> void {
-    constexpr int32_t PRINT_PRECISION = 7;
-    std::cout << std::setprecision(PRINT_PRECISION);
     std::cout << "Vector3(";
     std::cout << vec.x() << ", ";
     std::cout << vec.y() << ", ";
@@ -15,6 +13,9 @@ auto printVector(const tiny::math::Vector3<T>& vec) -> void {
 }
 
 auto main() -> int {
+    constexpr int32_t PRINT_PRECISION = 7;
+    std::cout << std::setprecision(PRINT_PRECISION);
+
     {
         using Vector3f = tiny::math::Vector3<tiny::math::float32_t>;
 
@@ -23,10 +24,19 @@ auto main() -> int {
         Vector3f vec_c(1.0F, 2.0F);        // NOLINT
         Vector3f vec_d(1.0F, 2.0F, 3.0F);  // NOLINT
 
-        printVector<tiny::math::float32_t>(vec_a);
-        printVector<tiny::math::float32_t>(vec_b);
-        printVector<tiny::math::float32_t>(vec_c);
-        printVector<tiny::math::float32_t>(vec_d);
+        std::cout << "Using vec3-float32 operator<<"
+                  << "\n";
+        std::cout << vec_a << "\n";
+        std::cout << vec_b << "\n";
+        std::cout << vec_c << "\n";
+        std::cout << vec_d << "\n";
+
+        std::cout << "Using vec3-float32 toString()"
+                  << "\n";
+        std::cout << vec_a.toString() << "\n";
+        std::cout << vec_b.toString() << "\n";
+        std::cout << vec_c.toString() << "\n";
+        std::cout << vec_d.toString() << "\n";
 
         static_assert(std::is_same<Vector3f::ElementType, float>::value,
                       "float32_t should be the same as vanilla 'float'");
@@ -34,15 +44,25 @@ auto main() -> int {
 
     {
         using Vector3d = tiny::math::Vector3<tiny::math::float64_t>;
+
         Vector3d vec_a;
         Vector3d vec_b(1.0);
         Vector3d vec_c(1.0, 2.0);       // NOLINT
         Vector3d vec_d(1.0, 2.0, 3.0);  // NOLINT
 
-        printVector<tiny::math::float64_t>(vec_a);
-        printVector<tiny::math::float64_t>(vec_b);
-        printVector<tiny::math::float64_t>(vec_c);
-        printVector<tiny::math::float64_t>(vec_d);
+        std::cout << "Using vec3-float64 operator<<"
+                  << "\n";
+        std::cout << vec_a << "\n";
+        std::cout << vec_b << "\n";
+        std::cout << vec_c << "\n";
+        std::cout << vec_d << "\n";
+
+        std::cout << "Using vec3-float64 toString()"
+                  << "\n";
+        std::cout << vec_a.toString() << "\n";
+        std::cout << vec_b.toString() << "\n";
+        std::cout << vec_c.toString() << "\n";
+        std::cout << vec_d.toString() << "\n";
 
         static_assert(std::is_same<Vector3d::ElementType, double>::value,
                       "float32_t should be the same as vanilla 'float'");
--- include/tinymath/vec3_t.hpp
+++ include/tinymath/vec3_t.hpp
@@ -2,7 +2,12 @@
 
 #include <array>
 #include <cstdint>
+#include <ios>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <tinymath/common.hpp>
+#include <type_traits>
 
 namespace tiny {
 namespace math {
@@ -46,12 +51,15 @@ class Vector3 {
         return m_Elements[index];
     }
 
+    auto toString() const -> std::string;
+
     constexpr auto ndim() const -> uint32_t { return VECTOR_NDIM; }
 
     constexpr auto buffer_size() const -> uint32_t { return BUFFER_SIZE; }
 
  private:
-    // @todo(wilbert): add union trick to handle xmm and ymm registers on SIMD
+    // @todo(wilbert): add union trick to handle xmm and ymm registers on
+    // SIMD
 
     BufferType m_Elements = {0, 0, 0, 1};
 };
@@ -63,6 +71,31 @@ auto operator+(const Vector3<Scalar_T>& lhs, const Vector3<Scalar_T>& rhs)
 template <typename Scalar_T>
 auto operator-(const Vector3<Scalar_T>& lhs, const Vector3<Scalar_T>& rhs)
     -> Vector3<Scalar_T>;
+
+template <typename Scalar_T>
+auto operator<<(std::ostream& stdout, const Vector3<Scalar_T>& src)
+    -> std::ostream& {
+    stdout << "(" << src.x() << ", " << src.y() << ", " << src.z() << ")";
+    return stdout;
+}
+
+template <typename Scalar_T>
+auto operator>>(std::istream& stdin, Vector3<Scalar_T>& dst) -> std::istream& {
+    // Based on ignition-math implementation https://bit.ly/3iqAVgS
+    Scalar_T x{};
+    Scalar_T y{};
+    Scalar_T z{};
+
+    stdin.setf(std::ios_base::skipws);
+    stdin >> x >> y >> z;
+    if (!stdin.fail()) {
+        dst.x() = x;
+        dst.y() = y;
+        dst.z() = z;
+    }
+
+    return stdin;
+}
 
 template <typename Scalar_T>
 Vector3<Scalar_T>::Vector3(Scalar_T x) {
@@ -86,6 +119,19 @@ Vector3<Scalar_T>::Vector3(Scalar_T x, Scalar_T y, Scalar_T z) {
     m_Elements[1] = y;
     m_Elements[2] = z;
     m_Elements[3] = 1;
+}
+
+template <typename Scalar_T>
+auto Vector3<Scalar_T>::toString() const -> std::string {
+    std::stringstream str_result;
+    if (std::is_same<ElementType, float>()) {
+        str_result << "Vector3f(" << x() << ", " << y() << ", " << z() << ")";
+    } else if (std::is_same<ElementType, double>()) {
+        str_result << "Vector3d(" << x() << ", " << y() << ", " << z() << ")";
+    } else {
+        str_result << "Vector3X(" << x() << ", " << y() << ", " << z() << ")";
+    }
+    return str_result.str();
 }
 
 }  // namespace math

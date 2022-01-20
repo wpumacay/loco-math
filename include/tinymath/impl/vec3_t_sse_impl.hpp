15803340af662b2b2920be1c1255e56a8f0a5226
gregor <wpumacay@gmail.com>
2022-01-20 06:13:06 -0500

    [feat(wip)]: updating SSE kernels

--- include/tinymath/impl/vec3_t_sse_impl.hpp
+++ include/tinymath/impl/vec3_t_sse_impl.hpp
@@ -70,42 +70,109 @@ TM_INLINE auto kernel_add_vec3(ArrayBuffer<T>& dst, const ArrayBuffer<T>& lhs,
     _mm_store_pd(dst.data() + 2, xmm_result_hi);
 }
 
-// ***************************************************************************//
-//   Implementations for single-precision floating point numbers (float32_t)  //
-// ***************************************************************************//
-using Vec3f = Vector3<float32_t>;
-using Array3f = Vec3f::BufferType;
-
-TM_INLINE auto kernel_add_v3f(Array3f& dst, const Array3f& lhs,
-                              const Array3f& rhs) -> void {
-    auto xmm_lhs = _mm_load_ps(lhs.data());
-    auto xmm_rhs = _mm_load_ps(rhs.data());
-    auto xmm_result = _mm_add_ps(xmm_lhs, xmm_rhs);
-    _mm_store_ps(dst.data(), xmm_result);
-}
-
-TM_INLINE auto kernel_sub_v3f(Array3f& dst, const Array3f& lhs,
-                              const Array3f& rhs) -> void {
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat32<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_sub_vec3(ArrayBuffer<T>& dst, const ArrayBuffer<T>& lhs,
+                               const ArrayBuffer<T>& rhs) -> void {
+    static_assert(std::is_same<float, T>::value, "We must be using f32");
     auto xmm_lhs = _mm_load_ps(lhs.data());
     auto xmm_rhs = _mm_load_ps(rhs.data());
     auto xmm_result = _mm_sub_ps(xmm_lhs, xmm_rhs);
     _mm_store_ps(dst.data(), xmm_result);
 }
 
-TM_INLINE auto kernel_scale_v3f(Array3f& dst, float32_t scale,
-                                const Array3f& vec) -> void {
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat64<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_sub_vec3(ArrayBuffer<T>& dst, const ArrayBuffer<T>& lhs,
+                               const ArrayBuffer<T>& rhs) -> void {
+    static_assert(std::is_same<double, T>::value, "We must be using f64");
+    auto xmm_lhs_lo = _mm_load_pd(lhs.data());
+    auto xmm_lhs_hi = _mm_load_pd(lhs.data() + 2);
+    auto xmm_rhs_lo = _mm_load_pd(rhs.data());
+    auto xmm_rhs_hi = _mm_load_pd(rhs.data() + 2);
+    auto xmm_result_lo = _mm_sub_pd(xmm_lhs_lo, xmm_rhs_lo);
+    auto xmm_result_hi = _mm_sub_pd(xmm_lhs_hi, xmm_rhs_hi);
+    _mm_store_pd(dst.data(), xmm_result_lo);
+    _mm_store_pd(dst.data() + 2, xmm_result_hi);
+}
+
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat32<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_scale_vec3(ArrayBuffer<T>& dst, T scale,
+                                 const ArrayBuffer<T>& vec) -> void {
+    static_assert(std::is_same<float, T>::value, "We must be using f32");
     auto xmm_scale = _mm_set1_ps(scale);
     auto xmm_vector = _mm_load_ps(vec.data());
     auto xmm_result = _mm_mul_ps(xmm_scale, xmm_vector);
     _mm_store_ps(dst.data(), xmm_result);
 }
 
-TM_INLINE auto kernel_hadamard_v3f(Array3f& dst, const Array3f& lhs,
-                                   const Array3f& rhs) -> void {
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat64<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_scale_vec3(ArrayBuffer<T>& dst, T scale,
+                                 const ArrayBuffer<T>& vec) -> void {
+    static_assert(std::is_same<double, T>::value, "We must be using f64");
+    auto xmm_scale = _mm_set1_pd(scale);
+    auto xmm_vector_lo = _mm_load_pd(vec.data());
+    auto xmm_vector_hi = _mm_load_pd(vec.data() + 2);
+    auto xmm_result_lo = _mm_mul_pd(xmm_scale, xmm_vector_lo);
+    auto xmm_result_hi = _mm_mul_pd(xmm_scale, xmm_vector_hi);
+    _mm_store_pd(dst.data(), xmm_result_lo);
+    _mm_store_pd(dst.data() + 2, xmm_result_hi);
+}
+
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat32<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_hadamard_vec3(ArrayBuffer<T>& dst,
+                                    const ArrayBuffer<T>& lhs,
+                                    const ArrayBuffer<T>& rhs) -> void {
+    static_assert(std::is_same<float, T>::value, "We must be using f32");
     auto xmm_lhs = _mm_load_ps(lhs.data());
     auto xmm_rhs = _mm_load_ps(rhs.data());
     _mm_store_ps(dst.data(), _mm_mul_ps(xmm_lhs, xmm_rhs));
 }
+
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat64<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_hadamard_vec3(ArrayBuffer<T>& dst,
+                                    const ArrayBuffer<T>& lhs,
+                                    const ArrayBuffer<T>& rhs) -> void {
+    static_assert(std::is_same<double, T>::value, "We must be using f64");
+    auto xmm_lhs_lo = _mm_load_pd(lhs.data());
+    auto xmm_lhs_hi = _mm_load_pd(lhs.data() + 2);
+    auto xmm_rhs_lo = _mm_load_pd(rhs.data());
+    auto xmm_rhs_hi = _mm_load_pd(rhs.data() + 2);
+    _mm_store_pd(dst.data(), _mm_mul_pd(xmm_lhs_lo, xmm_rhs_lo));
+    _mm_store_pd(dst.data() + 2, _mm_mul_pd(xmm_lhs_hi, xmm_rhs_hi));
+}
+
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat32<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_length_square_vec3(const ArrayBuffer<T>& vec) -> T {
+    static_assert(std::is_same<float, T>::value, "We must be using f32");
+    // @todo(wilbert)
+}
+
+template <typename T,
+          typename std::enable_if<CpuHasSSE<T>::value &&
+                                  IsFloat64<T>::value>::type* = nullptr>
+TM_INLINE auto kernel_length_square_vec3(const ArrayBuffer<T>& vec) -> T {
+    static_assert(std::is_same<double, T>::value, "We must be using f64");
+    // @todo(wilbert)
+}
+
+// ***************************************************************************//
+//   Implementations for single-precision floating point numbers (float32_t)  //
+// ***************************************************************************//
+using Vec3f = Vector3<float32_t>;
+using Array3f = Vec3f::BufferType;
 
 TM_INLINE auto kernel_length_square_v3f(const Array3f& vec) -> float32_t {
     // Implementation based on this post: https://bit.ly/3FyZF0n

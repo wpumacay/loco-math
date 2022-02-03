c159e51990de7dd6f357c3083fd65641848b9215
gregor <wpumacay@gmail.com>
2022-02-03 08:37:43 -0500

    [tests]: added more mat4 operations tests

--- tests/cpp/test_mat4_operations.cpp
+++ tests/cpp/test_mat4_operations.cpp
@@ -7,10 +7,8 @@ constexpr T RANGE_MIN = -10.0;
 template <typename T>
 constexpr T RANGE_MAX = 10.0;
 
-template <typename T, int N>
-auto GenRandom() -> T {
-    return GENERATE(take(N, random(RANGE_MIN<T>, RANGE_MAX<T>)));
-}
+// NOLINTNEXTLINE
+#define GenRandom(T, N) GENERATE(take(N, random(RANGE_MIN<T>, RANGE_MAX<T>)));
 
 // clang-format off
 template <typename T>
@@ -50,34 +48,56 @@ TEMPLATE_TEST_CASE("Matrix4 class (mat4_t) core operations", "[mat4_t][ops]",
     using Matrix4 = tiny::math::Matrix4<T>;
     using Vector4 = tiny::math::Vector4<T>;
 
-    // clang-format off
     // Generate first random matrix (2^8 cases possible values)
-    auto x00 = GenRandom<T, 2>(); auto x10 = GenRandom<T, 1>();
-    auto x01 = GenRandom<T, 2>(); auto x11 = GenRandom<T, 1>();
-    auto x02 = GenRandom<T, 2>(); auto x12 = GenRandom<T, 1>();
-    auto x03 = GenRandom<T, 2>(); auto x13 = GenRandom<T, 1>();
+    auto x00 = GenRandom(T, 2);
+    auto x01 = GenRandom(T, 2);
+    auto x02 = GenRandom(T, 2);
+    auto x03 = GenRandom(T, 2);
 
-    auto x20 = GenRandom<T, 1>(); auto x30 = GenRandom<T, 2>();
-    auto x21 = GenRandom<T, 1>(); auto x31 = GenRandom<T, 2>();
-    auto x22 = GenRandom<T, 1>(); auto x32 = GenRandom<T, 2>();
-    auto x23 = GenRandom<T, 1>(); auto x33 = GenRandom<T, 2>();
+    auto x10 = GenRandom(T, 1);
+    auto x11 = GenRandom(T, 1);
+    auto x12 = GenRandom(T, 1);
+    auto x13 = GenRandom(T, 1);
 
+    auto x20 = GenRandom(T, 1);
+    auto x21 = GenRandom(T, 1);
+    auto x22 = GenRandom(T, 1);
+    auto x23 = GenRandom(T, 1);
+
+    auto x30 = GenRandom(T, 2);
+    auto x31 = GenRandom(T, 2);
+    auto x32 = GenRandom(T, 2);
+    auto x33 = GenRandom(T, 2);
+
+    // clang-format off
     Matrix4 m_a(x00, x01, x02, x03,
                 x10, x11, x12, x13,
                 x20, x21, x22, x23,
                 x30, x31, x32, x33);
+    // clang-format on
 
     // Generate second random matrix (2^8 cases possible values)
-    auto y00 = GenRandom<T, 2>(); auto y10 = GenRandom<T, 1>();
-    auto y01 = GenRandom<T, 2>(); auto y11 = GenRandom<T, 1>();
-    auto y02 = GenRandom<T, 2>(); auto y12 = GenRandom<T, 1>();
-    auto y03 = GenRandom<T, 2>(); auto y13 = GenRandom<T, 1>();
+    auto y00 = GenRandom(T, 2);
+    auto y10 = GenRandom(T, 1);
+    auto y01 = GenRandom(T, 2);
+    auto y11 = GenRandom(T, 1);
 
-    auto y20 = GenRandom<T, 1>(); auto y30 = GenRandom<T, 2>();
-    auto y21 = GenRandom<T, 1>(); auto y31 = GenRandom<T, 2>();
-    auto y22 = GenRandom<T, 1>(); auto y32 = GenRandom<T, 2>();
-    auto y23 = GenRandom<T, 1>(); auto y33 = GenRandom<T, 2>();
+    auto y02 = GenRandom(T, 2);
+    auto y12 = GenRandom(T, 1);
+    auto y03 = GenRandom(T, 2);
+    auto y13 = GenRandom(T, 1);
 
+    auto y20 = GenRandom(T, 1);
+    auto y30 = GenRandom(T, 2);
+    auto y21 = GenRandom(T, 1);
+    auto y31 = GenRandom(T, 2);
+
+    auto y22 = GenRandom(T, 1);
+    auto y32 = GenRandom(T, 2);
+    auto y23 = GenRandom(T, 1);
+    auto y33 = GenRandom(T, 2);
+
+    // clang-format off
     Matrix4 m_b(y00, y01, y02, y03,
                 y10, y11, y12, y13,
                 y20, y21, y22, y23,
@@ -118,8 +138,8 @@ TEMPLATE_TEST_CASE("Matrix4 class (mat4_t) core operations", "[mat4_t][ops]",
         // clang-format on
     }
     SECTION("Matrix-Scalar product") {
-        auto scale_1 = GenRandom<T, 1>();
-        auto scale_2 = GenRandom<T, 1>();
+        auto scale_1 = GenRandom(T, 1);
+        auto scale_2 = GenRandom(T, 1);
 
         auto mat_scaled_1 = scale_1 * m_a;
         auto mat_scaled_2 = m_b * scale_2;
@@ -139,5 +159,53 @@ TEMPLATE_TEST_CASE("Matrix4 class (mat4_t) core operations", "[mat4_t][ops]",
             y20 * scale_2, y21 * scale_2, y22 * scale_2, y23 * scale_2,
             y30 * scale_2, y31 * scale_2, y32 * scale_2, y33 * scale_2);
         // clang-format on
+    }
+    SECTION("Matrix-Matrix product") {
+        // clang-format off
+        // Fixed test-case
+        Matrix4 m_1(-10.0, -6.0, -6.0,  2.0,
+                    -8.0,  -6.0, -6.0, -5.0,
+                     7.0, -10.0,  5.0,  7.0,
+                    -2.0,  -9.0, -8.0,  6.0);
+
+        Matrix4 m_2(7.0,  7.0, -6.0,  3.0,
+                   -7.0,  1.0,  8.0,  5.0,
+                    7.0, -2.0,  0.0, -4.0,
+                   -9.0,  2.0,  5.0, -3.0);
+
+        auto mat_mul = m_1 * m_2;
+        FuncAllClose<T>(mat_mul,
+            -88.0, -60.0,  22.0, -42.0,
+            -11.0, -60.0, -25.0, -15.0,
+             91.0,  43.0, -87.0, -70.0,
+            -61.0,   5.0, -30.0, -37.0);
+        // clang-format on
+
+        // Test-cases using the random matrices D:
+        auto mat_mul_ab = m_a * m_b;
+        // clang-format off
+        FuncAllClose<T>(mat_mul_ab,
+            // First row
+            x00 * y00 + x01 * y10 + x02 * y20 + x03 * y30,
+            x00 * y01 + x01 * y11 + x02 * y21 + x03 * y31,
+            x00 * y02 + x01 * y12 + x02 * y22 + x03 * y32,
+            x00 * y03 + x01 * y13 + x02 * y23 + x03 * y33,
+            // Second row
+            x10 * y00 + x11 * y10 + x12 * y20 + x13 * y30,
+            x10 * y01 + x11 * y11 + x12 * y21 + x13 * y31,
+            x10 * y02 + x11 * y12 + x12 * y22 + x13 * y32,
+            x10 * y03 + x11 * y13 + x12 * y23 + x13 * y33,
+            // Third row
+            x20 * y00 + x21 * y10 + x22 * y20 + x23 * y30,
+            x20 * y01 + x21 * y11 + x22 * y21 + x23 * y31,
+            x20 * y02 + x21 * y12 + x22 * y22 + x23 * y32,
+            x20 * y03 + x21 * y13 + x22 * y23 + x23 * y33,
+            // Fourth row
+            x30 * y00 + x31 * y10 + x32 * y20 + x33 * y30,
+            x30 * y01 + x31 * y11 + x32 * y21 + x33 * y31,
+            x30 * y02 + x31 * y12 + x32 * y22 + x33 * y32,
+            x30 * y03 + x31 * y13 + x32 * y23 + x33 * y33);
+        // clang-format on
+        // @todo(wilbert): test with poorly conditioned matrices
     }
 }

8ccbfd25b2cfbd8996acf00e05fcbb79346fed45
wpumacay <wpumacay@gmail.com>
2022-02-11 16:43:53 -0500

    [fix]: adds patch for asm-checking on msvc

--- examples/cpp/operations/ex_vec3f_ops.cpp
+++ examples/cpp/operations/ex_vec3f_ops.cpp
@@ -8,14 +8,18 @@ TM_NEVER_INLINE auto check_operator_add() -> float {
     Vector3f vec_b;
     std::cin >> vec_a;
     std::cin >> vec_b;
+#ifndef TINYMATH_COMPILER_MSVC
     __asm__(
         "check_operator_add_start:;"
         "nop;");
+#endif
     // Use operator+ (check the dissassembly)
     auto vec_sum = vec_a + vec_b;
+#ifndef TINYMATH_COMPILER_MSVC
     __asm__(
         "check_operator_add_end:;"
         "nop;");
+#endif
     return vec_sum[0] + vec_sum[1] + vec_sum[2];
 }
 
@@ -24,14 +28,18 @@ TM_NEVER_INLINE auto check_operator_sub() -> float {
     Vector3f vec_b;
     std::cin >> vec_a;
     std::cin >> vec_b;
+#ifndef TINYMATH_COMPILER_MSVC
     __asm__(
         "check_operator_sub_start:;"
         "nop;");
+#endif
     // Use operator- (check the dissassembly)
     auto vec_sub = vec_a - vec_b;
+#ifndef TINYMATH_COMPILER_MSVC
     __asm__(
         "check_operator_sub_end:;"
         "nop;");
+#endif
     return vec_sub[0] + vec_sub[1] + vec_sub[2];
 }
 
@@ -40,14 +48,18 @@ TM_NEVER_INLINE auto check_operator_hadamard() -> float {
     Vector3f vec_b;
     std::cin >> vec_a;
     std::cin >> vec_b;
+#ifndef TINYMATH_COMPILER_MSVC
     __asm__(
         "check_operator_hadamard_start:;"
         "nop;");
+#endif
     // Use operator- (check the dissassembly)
     auto vec_hm = vec_a * vec_b;
+#ifndef TINYMATH_COMPILER_MSVC
     __asm__(
         "check_operator_hadamard_end:;"
         "nop;");
+#endif
     return vec_hm[0] + vec_hm[1] + vec_hm[2];
 }
 

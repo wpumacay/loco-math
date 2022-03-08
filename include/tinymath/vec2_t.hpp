9e04a9b10b2c66dfe72961acd72dec8315e32b07
gregor <wpumacay@gmail.com>
2022-03-08 12:21:49 -0500

    [feat]: adds completed vec2 API (ops later)

--- include/tinymath/vec2_t.hpp
+++ include/tinymath/vec2_t.hpp
@@ -26,14 +26,16 @@ namespace math {
 ///
 /// This is a class that represents a 2d-vector with entries x, y of some
 /// scalar floating-point type. Its storage is a buffer of the given scalar
-/// type, and it's aligned accordingly in order to use the aligned versions of
-/// some SIMD instructions (when using either SSE or AVX intrinsics).
+/// type; however, it's not aligned for SIMD load/store instructions (tradeoff
+/// between adding padding/size and usage of aligned load/store). So, SIMD
+/// kernels are build using unaligned load/store operations (unlike friends like
+/// vec3 and vec4 types)
 template <typename Scalar_T>
 struct Vector2 {
     /// Number of scalars used in the storage of the vector
     static constexpr uint32_t BUFFER_SIZE = 2;
     /// Number of scalar dimensions of the vector
-    static constexpr uint32_t VECTOR_NDIM = 3;
+    static constexpr uint32_t VECTOR_NDIM = 2;
 
     /// Type alias of the vector
     using Type = Vector2<Scalar_T>;
@@ -63,10 +65,108 @@ struct Vector2 {
         std::copy(values.begin(), values.end(), m_Elements.data());
     }
 
+    /// Returns a mutable reference to the x-component of the vector
+    auto x() -> Scalar_T& { return m_Elements[0]; }
+
+    /// Returns a mutable reference to the y-component of the vector
+    auto y() -> Scalar_T& { return m_Elements[1]; }
+
+    /// Returns an unmutable reference to the x-component of the vector
+    auto x() const -> const Scalar_T& { return m_Elements[0]; }
+
+    /// Returns an unmutable reference to the y-component of the vector
+    auto y() const -> const Scalar_T& { return m_Elements[1]; }
+
+    /// Returns a mutable reference to the underlying storage of the vector
+    auto elements() -> BufferType& { return m_Elements; }
+
+    /// Returns an unmutable reference to the underlying storage of the vector
+    auto elements() const -> const BufferType& { return m_Elements; }
+
+    /// Returns a pointer to the data of the underlying storage in use
+    auto data() -> Scalar_T* { return m_Elements.data(); }
+
+    /// Reeturns a const-pointer to the data of the underlying storage in use
+    auto data() const -> const Scalar_T* { return m_Elements.data(); }
+
+    /// Returns a mutable reference to the requested entry of the vector
+    auto operator[](uint32_t index) -> Scalar_T& { return m_Elements[index]; }
+
+    /// Returns an unmutable reference to the requested entry of the vector
+    auto operator[](uint32_t index) const -> const Scalar_T& {
+        return m_Elements[index];
+    }
+
+    /// Returns a printable string-representation of the vector
+    auto toString() const -> std::string {
+        std::stringstream str_result;
+        if (std::is_same<ElementType, float>()) {
+            str_result << "Vector2f(" << x() << ", " << y() << ")";
+        } else if (std::is_same<ElementType, double>()) {
+            str_result << "Vector2d(" << x() << ", " << y() << ")";
+        } else {
+            str_result << "Vector2X(" << x() << ", " << y() << ")";
+        }
+        return str_result.str();
+    }
+
+    /// Returns the number of dimensions of the vector (Vector2 <-> 2 scalars)
+    constexpr auto ndim() const -> uint32_t { return VECTOR_NDIM; }
+
+    /// Returns the number of scalars used by the storage of the vector
+    constexpr auto buffer_size() const -> uint32_t { return BUFFER_SIZE; }
+
+    /// Returns the size (in bytes) of the vector
+    static constexpr auto num_bytes_size() -> uint32_t { return sizeof(Type); }
+
+    /// Returns the alignment (in bytes) of the vector
+    static constexpr auto num_bytes_alignment() -> uint32_t {
+        return alignof(Type);
+    }
+
  private:
     /// Storage of the vector's scalars
     alignas(sizeof(Scalar_T) * BUFFER_SIZE) BufferType m_Elements = {0, 0};
 };
+
+/// \brief Prints the given vector to the given output stream
+///
+/// \tparam T Type of scalar used by the vector operand
+///
+/// \param[in,out] output_stream The output stream to write the vector to
+/// \param[in] src The vector we want to print to the output stream
+/// \returns A reference to the modified output stream (to concatenate calls)
+template <typename T,
+          typename std::enable_if<IsScalar<T>::value>::type* = nullptr>
+auto operator<<(std::ostream& output_stream, const Vector2<T>& src)
+    -> std::ostream& {
+    output_stream << "(" << src.x() << ", " << src.y() << ")";
+    return output_stream;
+}
+
+/// \brief Reads a vector from the given input stream
+///
+/// \tparam T Type of scalar used by the vector operand
+///
+/// \param[in,out] input_stream The input stream from which to read the vector
+/// \param[out] dst The vector in which to place the read values
+/// \returns A reference to the modified input stream (to concatenate calls)
+template <typename T,
+          typename std::enable_if<IsScalar<T>::value>::type* = nullptr>
+auto operator>>(std::istream& input_stream, Vector2<T>& dst) -> std::istream& {
+    // Based on ignition-math implementation https://bit.ly/3iqAVgS
+    T x{};
+    T y{};
+
+    input_stream.setf(std::ios_base::skipws);
+    input_stream >> x >> y;
+    if (!input_stream.fail()) {
+        dst.x() = x;
+        dst.y() = y;
+    }
+
+    return input_stream;
+}
 
 }  // namespace math
 }  // namespace tiny

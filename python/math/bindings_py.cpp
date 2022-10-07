15109f4a1e85fec5888e01e3b47a58ae02834211
gregor <wpumacay@gmail.com>
2022-10-06 20:06:06 -0500

    [feat]: adds bindings for mat3 types

--- python/math/bindings_py.cpp
+++ python/math/bindings_py.cpp
@@ -6,6 +6,7 @@
 #include <vec3_py.hpp>
 #include <vec4_py.hpp>
 #include <mat2_py.hpp>
+#include <mat3_py.hpp>
 #include <mat4_py.hpp>
 
 namespace py = pybind11;
@@ -20,6 +21,8 @@ PYBIND11_MODULE(_math, m) {
     loco::math::bindings_vector4<loco::math::float64_t>(m, "Vector4d");
     loco::math::bindings_matrix2<loco::math::float32_t>(m, "Matrix2f");
     loco::math::bindings_matrix2<loco::math::float64_t>(m, "Matrix2d");
+    loco::math::bindings_matrix3<loco::math::float32_t>(m, "Matrix3f");
+    loco::math::bindings_matrix3<loco::math::float64_t>(m, "Matrix3d");
     loco::math::bindings_matrix4<loco::math::float32_t>(m, "Matrix4f");
     loco::math::bindings_matrix4<loco::math::float64_t>(m, "Matrix4d");
 }
---
+++ python/math/mat3_py.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <cstring>
+#include <stdexcept>
+#include <utility>
+
+#include <pybind11/attr.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/numpy.h>
+
+#include <loco/math/mat3_t_impl.hpp>
+
+#include <common_py.hpp>
+#include <conversions_py.hpp>
+
+namespace py = pybind11;
+
+namespace loco {
+namespace math {
+
+template <typename T>
+using SFINAE_MAT3_BINDINGS = typename std::enable_if<IsScalar<T>::value>::type*;
+
+template <typename T, SFINAE_MAT3_BINDINGS<T> = nullptr>
+// NOLINTNEXTLINE
+auto bindings_matrix3(py::module& m, const char* class_name) -> void {
+    using Class = Matrix3<T>;
+    using Column = typename Matrix3<T>::ColumnType;
+    py::class_<Class>(m, class_name, py::buffer_protocol())
+        .def(py::init<>())
+        .def(py::init<Column, Column, Column>())
+        // clang-format off
+        MATRIX_BUFFER_PROTOCOL(3, T)
+        MATRIX_OPERATORS(T)
+        // cppcheck-suppress constParameter
+        MATRIX_GETSET_ITEM(3, T)
+        // clang-format on
+        .def("__repr__", [](const Class& self) -> py::str {
+            // clang-format off
+            return py::str("Matrix3{}([[{},{},{}],\n"
+                           "          [{},{},{}],\n"
+                           "          [{},{},{}]]")
+                .format(IsFloat32<T>::value ? "f" : "d",
+                    self(0, 0), self(0, 1), self(0, 2),
+                    self(1, 0), self(1, 1), self(1, 2),
+                    self(2, 0), self(2, 1), self(2, 2));
+
+            // clang-format on
+        });
+}
+
+}  // namespace math
+}  // namespace loco

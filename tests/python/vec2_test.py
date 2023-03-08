2e8ccf809d468d33ebfdc63f5a226b647d0f0638
gregor <wpumacay@gmail.com>
2023-03-08 11:52:44 -0500

    tests(update): adds more tests for vec2 operations

--- tests/python/vec2_test.py
+++ tests/python/vec2_test.py
@@ -32,19 +32,43 @@ def test_vec2_accessors():
     assert (vec.x == 3.0) and (vec.y == 7.0)
     # Can get elements using __getitem__ as well
     assert (vec[0] == 3.0) and (vec[1] == 7.0)
+    # Can also update the entries using either of the two accessors
+    vec.x, vec.y = 1.0, 2.0
+    assert (vec.x == 1.0) and (vec.y == 2.0)
 
 class TestVec2Operators:
     def test_comparison_operator(self):
-        pass
+        vec_a = m3d.Vector2f(1.0, 2.0)
+        vec_b = m3d.Vector2f(1.0, 2.0)
+        # Comparison __eq__ internally calls == operator, which uses an epsilon
+        assert (vec_a == vec_b)
+
+        vec_b.x, vec_b.y =1.1, 2.1
+        # Comparison __neq__ internally calls != operator, which uses an epsilon
+        assert (vec_a != vec_b)
 
     def test_vector_addition(self):
-        pass
+        vec_a = m3d.Vector2f(3.0, 7.0)
+        vec_b = m3d.Vector2f(4.0, 5.0)
+        vec_sum = vec_a + vec_b
+        assert (vec_sum.x == 7.0) and (vec_sum.y == 12.0)
 
     def test_vector_substraction(self):
-        pass
+        vec_a = m3d.Vector2f(3.0, 7.0)
+        vec_b = m3d.Vector2f(4.0, 5.0)
+        vec_diff = vec_a - vec_b
+        assert (vec_diff.x == -1.0) and (vec_diff.y == 2.0)
 
     def test_vector_elementwise_product(self):
-        pass
+        vec_a = m3d.Vector2f(3.0, 7.0)
+        vec_b = m3d.Vector2f(4.0, 5.0)
+        vec_elm_mul = vec_a * vec_b
+        assert (vec_elm_mul.x == 12.0) and (vec_elm_mul.y == 35.0)
 
     def test_vector_scalar_product(self):
-        pass
+        vec = m3d.Vector2f(4.0, 8.0)
+        scale = 3.5
+        vec_scaled = vec * scale
+        assert (vec_scaled.x == 14.0) and (vec_scaled.y == 28.0)
+        vec_scaled = scale * vec
+        assert (vec_scaled.x == 14.0) and (vec_scaled.y == 28.0)

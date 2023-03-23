3fb091f2dc4d26bbff29affff27b3c44caa2878a
gregor <wpumacay@gmail.com>
2023-03-23 17:13:51 -0500

    update(feature): make sure quats are normalized when using pose3d

--- include/math/pose3d_t.hpp
+++ include/math/pose3d_t.hpp
@@ -54,7 +54,7 @@ auto Pose3d<T>::toMatrix() const -> Mat4 {
 
 template <typename T>
 auto Pose3d<T>::operator*(const Pose3d<T>& rhs) const -> Pose3d<T> {
-    auto res_orientation = this->orientation * rhs->orientation;
+    auto res_orientation = (this->orientation * rhs->orientation).normalized();
     auto res_position =
         this->position + this->orientation.rotate(rhs->position);
     return Pose3d<T>(res_position, res_orientation);

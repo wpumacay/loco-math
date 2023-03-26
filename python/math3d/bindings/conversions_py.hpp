56d606939e024f8ffd33248515ebbe51828af532
gregor <wpumacay@gmail.com>
2023-03-26 14:07:38 -0500

    refactor(bindings): refactored math3d to nparray and buffers conversions

--- python/math3d/bindings/conversions_py.hpp
+++ python/math3d/bindings/conversions_py.hpp
@@ -18,169 +18,121 @@ namespace py = pybind11;
 #pragma warning(disable : 4127)
 #endif
 
+// clang-format off
+
+// NOLINTNEXTLINE
+#define VECTOR_TO_NPARRAY(VecCls, xvec)                             \
+    auto array_np = py::array_t<T>(VecCls::VECTOR_SIZE);            \
+    memcpy(array_np.request().ptr, xvec.data(), sizeof(VecCls));    \
+    return array_np
+
+// NOLINTNEXTLINE
+#define NPARRAY_TO_VECTOR(VecCls, xarray_np)                                \
+    auto array_buffer_info = xarray_np.request();                           \
+    if (array_buffer_info.size != VecCls::VECTOR_SIZE) {                    \
+        throw std::runtime_error("Incompatible array size, expected " +     \
+                                 std::to_string(VecCls::VECTOR_SIZE) +      \
+                                 " elements");                              \
+    }                                                                       \
+    VecCls vec;                                                             \
+    memcpy(vec.data(), array_buffer_info.ptr, sizeof(VecCls));              \
+    return vec
+
+// NOLINTNEXTLINE
+#define BUFFER_TO_VECTOR(VecCls, xbuff)                                     \
+    constexpr size_t SIZE_N = VecCls::VECTOR_SIZE;                          \
+    using Type = typename VecCls::ElementType;                              \
+    py::buffer_info info = xbuff.request();                                 \
+    if (IsFloat32<Type>::value &&                                           \
+        (info.format != py::format_descriptor<Type>::format())) {           \
+        throw std::runtime_error(                                           \
+            "Incompatible format: expected float (float32) array");         \
+    }                                                                       \
+    if (IsFloat64<Type>::value &&                                           \
+        (info.format != py::format_descriptor<Type>::format())) {           \
+        throw std::runtime_error(                                           \
+            "Incompatible format: expected double (float64) array");        \
+    }                                                                       \
+    bool is_valid_shape;                                                    \
+    if (info.ndim == 1) {                                                   \
+        is_valid_shape = (info.shape[0] == SIZE_N);                         \
+    } else if (info.ndim == 2) {                                            \
+        is_valid_shape = (info.shape[0] == SIZE_N ||                        \
+                          info.shape[1] == SIZE_N);                         \
+    } else {                                                                \
+        is_valid_shape = false;                                             \
+    }                                                                       \
+    if (!is_valid_shape) {                                                  \
+        throw std::runtime_error(                                           \
+            "Incompatible shape: expected (,n), (1,n), (n,1)");             \
+    }                                                                       \
+    VecCls vec;                                                             \
+    memcpy(vec.data(), info.ptr, sizeof(Type) * SIZE_N);                    \
+    return vec
+
+// clang-format on
+
 namespace math {
 
 template <typename T>
 using SFINAE_CONVERSIONS_BINDINGS =
     typename std::enable_if<IsScalar<T>::value>::type*;
 
+// -------------------------------------------------------------------------- //
+//                    Conversions from Math3d to NumPy                        //
+// -------------------------------------------------------------------------- //
+
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto vec2_to_nparray(const Vector2<T>& vec) -> py::array_t<T> {
-    auto array_np = py::array_t<T>(Vector2<T>::VECTOR_SIZE);
-    memcpy(array_np.request().ptr, vec.data(), sizeof(Vector2<T>));
-    return array_np;
+    VECTOR_TO_NPARRAY(Vector2<T>, vec);
 }
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto vec3_to_nparray(const Vector3<T>& vec) -> py::array_t<T> {
-    auto array_np = py::array_t<T>(Vector3<T>::VECTOR_SIZE);
-    memcpy(array_np.request().ptr, vec.data(), sizeof(Vector3<T>));
-    return array_np;
+    VECTOR_TO_NPARRAY(Vector3<T>, vec);
 }
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto vec4_to_nparray(const Vector4<T>& vec) -> py::array_t<T> {
-    auto array_np = py::array_t<T>(Vector4<T>::VECTOR_SIZE);
-    memcpy(array_np.request().ptr, vec.data(), sizeof(Vector4<T>));
-    return array_np;
+    VECTOR_TO_NPARRAY(Vector4<T>, vec);
 }
+
+// -------------------------------------------------------------------------- //
+//                    Conversions from NumPy to Math3d                        //
+// -------------------------------------------------------------------------- //
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto nparray_to_vec2(const py::array_t<T>& array_np) -> Vector2<T> {
-    auto array_buffer_info = array_np.request();
-    if (array_buffer_info.size != Vector2<T>::VECTOR_SIZE) {
-        throw std::runtime_error("Incompatible array size, expected " +
-                                 std::to_string(Vector2<T>::VECTOR_SIZE) +
-                                 " elements");
-    }
-
-    Vector2<T> vec;
-    memcpy(vec.data(), array_buffer_info.ptr, sizeof(Vector2<T>));
-    return vec;
+    NPARRAY_TO_VECTOR(Vector2<T>, array_np);
 }
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto nparray_to_vec3(const py::array_t<T>& array_np) -> Vector3<T> {
-    auto array_buffer_info = array_np.request();
-    if (array_buffer_info.size != Vector3<T>::VECTOR_SIZE) {
-        throw std::runtime_error("Incompatible array size, expected " +
-                                 std::to_string(Vector3<T>::VECTOR_SIZE) +
-                                 " elements");
-    }
-
-    Vector3<T> vec;
-    memcpy(vec.data(), array_buffer_info.ptr, sizeof(Vector3<T>));
-    return vec;
+    NPARRAY_TO_VECTOR(Vector3<T>, array_np);
 }
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto nparray_to_vec4(const py::array_t<T>& array_np) -> Vector4<T> {
-    auto array_buffer_info = array_np.request();
-    if (array_buffer_info.size != Vector4<T>::VECTOR_SIZE) {
-        throw std::runtime_error("Incompatible array size, expected " +
-                                 std::to_string(Vector4<T>::VECTOR_SIZE) +
-                                 " elements");
-    }
-
-    Vector4<T> vec;
-    memcpy(vec.data(), array_buffer_info.ptr, sizeof(Vector4<T>));
-    return vec;
+    NPARRAY_TO_VECTOR(Vector4<T>, array_np);
 }
+
+// -------------------------------------------------------------------------- //
+//                 Conversions from Buffer Protocol to Math3d                 //
+// -------------------------------------------------------------------------- //
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto buffer_to_vec4(const py::buffer& buff) -> Vector4<T> {
-    py::buffer_info info = buff.request();
-    if (IsFloat32<T>::value &&
-        (info.format != py::format_descriptor<T>::format())) {
-        throw std::runtime_error(
-            "Incompatible format: expected float (float32) array");
-    }
-    if (IsFloat64<T>::value &&
-        (info.format != py::format_descriptor<T>::format())) {
-        throw std::runtime_error(
-            "Incompatible format: expected double (float64) array");
-    }
-    // NOLINTNEXTLINE TODO(wilbert): cppcheck might be giving a false positive?
-    bool is_valid_shape;
-    if (info.ndim == 1) {
-        is_valid_shape = (info.shape[0] == Vector4<T>::VECTOR_SIZE);
-    } else if (info.ndim == 2) {
-        is_valid_shape = (info.shape[0] == Vector4<T>::VECTOR_SIZE ||
-                          info.shape[1] == Vector4<T>::VECTOR_SIZE);
-    } else {
-        is_valid_shape = false;
-    }
-    if (!is_valid_shape) {
-        throw std::runtime_error(
-            "Incompatible shape: expected (,n), (1,n), (n,1)");
-    }
-    Vector4<T> vec;
-    memcpy(vec.data(), info.ptr, sizeof(T) * Vector4<T>::VECTOR_SIZE);
-    return vec;
+    BUFFER_TO_VECTOR(Vector4<T>, buff);
 }
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto buffer_to_vec3(const py::buffer& buff) -> Vector3<T> {
-    py::buffer_info info = buff.request();
-    if (IsFloat32<T>::value &&
-        (info.format != py::format_descriptor<T>::format())) {
-        throw std::runtime_error(
-            "Incompatible format: expected float (float32) array");
-    }
-    if (IsFloat64<T>::value &&
-        (info.format != py::format_descriptor<T>::format())) {
-        throw std::runtime_error(
-            "Incompatible format: expected double (float64) array");
-    }
-    // NOLINTNEXTLINE TODO(wilbert): cppcheck might be giving a false positive?
-    bool is_valid_shape;
-    if (info.ndim == 1) {
-        is_valid_shape = (info.shape[0] == Vector3<T>::VECTOR_SIZE);
-    } else if (info.ndim == 2) {
-        is_valid_shape = (info.shape[0] == Vector3<T>::VECTOR_SIZE ||
-                          info.shape[1] == Vector3<T>::VECTOR_SIZE);
-    } else {
-        is_valid_shape = false;
-    }
-    if (!is_valid_shape) {
-        throw std::runtime_error(
-            "Incompatible shape: expected (,n), (1,n), (n,1)");
-    }
-    Vector3<T> vec;
-    memcpy(vec.data(), info.ptr, sizeof(T) * Vector3<T>::VECTOR_SIZE);
-    return vec;
+    BUFFER_TO_VECTOR(Vector3<T>, buff);
 }
 
 template <typename T, SFINAE_CONVERSIONS_BINDINGS<T> = nullptr>
 inline auto buffer_to_vec2(const py::buffer& buff) -> Vector2<T> {
-    py::buffer_info info = buff.request();
-    if (IsFloat32<T>::value &&
-        (info.format != py::format_descriptor<T>::format())) {
-        throw std::runtime_error(
-            "Incompatible format: expected float (float32) array");
-    }
-    if (IsFloat64<T>::value &&
-        (info.format != py::format_descriptor<T>::format())) {
-        throw std::runtime_error(
-            "Incompatible format: expected double (float64) array");
-    }
-    // NOLINTNEXTLINE TODO(wilbert): cppcheck might be giving a false positive?
-    bool is_valid_shape;
-    if (info.ndim == 1) {
-        is_valid_shape = (info.shape[0] == Vector2<T>::VECTOR_SIZE);
-    } else if (info.ndim == 2) {
-        is_valid_shape = (info.shape[0] == Vector2<T>::VECTOR_SIZE ||
-                          info.shape[1] == Vector2<T>::VECTOR_SIZE);
-    } else {
-        is_valid_shape = false;
-    }
-    if (!is_valid_shape) {
-        throw std::runtime_error(
-            "Incompatible shape: expected (,n), (1,n), (n,1)");
-    }
-    Vector2<T> vec;
-    memcpy(vec.data(), info.ptr, sizeof(T) * Vector2<T>::VECTOR_SIZE);
-    return vec;
+    BUFFER_TO_VECTOR(Vector2<T>, buff);
 }
 
 }  // namespace math

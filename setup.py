1a308fd0f8bd0b8df0422e36d05f0c5e6e399d53
gregor <wpumacay@gmail.com>
2023-04-19 16:18:39 -0500

    chore(versioning): Bump version number to v0.4.3

--- setup.py
+++ setup.py
@@ -165,7 +165,7 @@ if os.path.exists("README.md"):
 
 setup(
     name="Math3d",
-    version="0.4.2",
+    version="0.4.3",
     description="A math library for small vectors and matrices",
     long_description=long_description,
     long_description_content_type="text/markdown",

aae1cabdbfa44ea7086fa73b29c8c4126a748e09
gregor <wpumacay@gmail.com>
2023-07-03 15:05:09 -0500

    (tests): adds checks for mat4 operators

--- tests/python/mat4_test.py
+++ tests/python/mat4_test.py
@@ -103,7 +103,7 @@ class TestMat4Constructors:
         (m3d.Matrix4d, m3d.Vector4d, np.float64),
     ],
 )
-class TestMat2Accessors:
+class TestMat4Accessors:
     def test_get_column(
         self, Mat4: Matrix4Cls, Vec4: Vector4Cls, FloatType: type
     ) -> None:
@@ -212,3 +212,202 @@ class TestMat2Accessors:
                                 [13.1, 14.1, 15.1, 16.1]], dtype=FloatType)
         # fmt: on
         assert mat4_all_close(mat, expected_np)
+
+
+@pytest.mark.parametrize(
+    "Mat4, Vec4, FloatType",
+    [
+        (m3d.Matrix4f, m3d.Vector4f, np.float32),
+        (m3d.Matrix4d, m3d.Vector4d, np.float64),
+    ],
+)
+class TestMat4Operators:
+    def test_comparison_operator(
+        self, Mat4: Matrix4Cls, Vec4: Vector4Cls, FloatType: type
+    ) -> None:
+        # fmt: off
+        mat_a = Mat4( 1.0,  2.0,  3.0,  4.0,
+                      5.0,  6.0,  7.0,  8.0,
+                      9.0, 10.0, 11.0, 12.0,
+                     13.0, 14.0, 15.0, 16.0)
+        mat_b = Mat4( 1.0,  2.0,  3.0,  4.0,
+                      5.0,  6.0,  7.0,  8.0,
+                      9.0, 10.0, 11.0, 12.0,
+                     13.0, 14.0, 15.0, 16.0)
+        # fmt: on
+
+        # Checking comparison operator (__eq__)
+        assert mat_a == mat_b
+
+        # Update the matrices so they don't match
+        mat_a[0, 0], mat_b[0, 0] = 1.1, 2.1
+
+        # Checking neg. comparison  operator (__neq__)
+        assert mat_a != mat_b
+
+    def test_matrix_addition(
+        self, Mat4: Matrix4Cls, Vec4: Vector4Cls, FloatType: type
+    ) -> None:
+        # Testing against some hard-coded matrices
+        # fmt: off
+        mat_a = Mat4( 1.0,  2.0,  3.0,  4.0,
+                      5.0,  6.0,  7.0,  8.0,
+                      9.0, 10.0, 11.0, 12.0,
+                     13.0, 14.0, 15.0, 16.0)
+        mat_b = Mat4( 2.0,  3.0,  5.0,  7.0,
+                     11.0, 13.0, 17.0, 19.0,
+                     23.0, 29.0, 31.0, 37.0,
+                     41.0, 43.0, 47.0, 53.0)
+        mat_c = mat_a + mat_b
+        expected_c = Mat4( 3.0,  5.0,  8.0, 11.0,
+                          16.0, 19.0, 24.0, 27.0,
+                          32.0, 39.0, 42.0, 49.0,
+                          54.0, 57.0, 62.0, 69.0)
+        # fmt: on
+        assert type(mat_c) is Mat4
+        assert mat_c == expected_c
+
+        # Testing against some randomly sampled matrices
+        for _ in range(NUM_RANDOM_SAMPLES):
+            np_a = np.random.randn(4, 4).astype(FloatType)
+            np_b = np.random.randn(4, 4).astype(FloatType)
+            np_c = np_a + np_b
+
+            mat_a, mat_b = Mat4(np_a), Mat4(np_b)
+            mat_c = mat_a + mat_b
+            # Check that we're doing what numpy does for addition
+            assert mat4_all_close(mat_c, np_c)
+
+    def test_matrix_substraction(
+        self, Mat4: Matrix4Cls, Vec4: Vector4Cls, FloatType: type
+    ) -> None:
+        # Testing against some hard-coded matrices
+        # fmt: off
+        mat_a = Mat4( 1.0,  2.0,  3.0,  4.0,
+                      5.0,  6.0,  7.0,  8.0,
+                      9.0, 10.0, 11.0, 12.0,
+                     13.0, 14.0, 15.0, 16.0)
+        mat_b = Mat4( 2.0,  3.0,  5.0,  7.0,
+                     11.0, 13.0, 17.0, 19.0,
+                     23.0, 29.0, 31.0, 37.0,
+                     41.0, 43.0, 47.0, 53.0)
+        mat_c = mat_a - mat_b
+        expected_c = Mat4( -1.0,  -1.0,  -2.0,  -3.0,
+                           -6.0,  -7.0, -10.0, -11.0,
+                          -14.0, -19.0, -20.0, -25.0,
+                          -28.0, -29.0, -32.0, -37.0)
+        # fmt: on
+        assert type(mat_c) is Mat4
+        assert mat_c == expected_c
+
+        # Testing against some randomly sampled matrices
+        for _ in range(NUM_RANDOM_SAMPLES):
+            np_a = np.random.randn(4, 4).astype(FloatType)
+            np_b = np.random.randn(4, 4).astype(FloatType)
+            np_c = np_a - np_b
+
+            mat_a, mat_b = Mat4(np_a), Mat4(np_b)
+            mat_c = mat_a - mat_b
+            # Check that we're doing what numpy does for addition
+            assert mat4_all_close(mat_c, np_c)
+
+    def test_matrix_scalar_product(
+        self, Mat4: Matrix4Cls, Vec4: Vector4Cls, FloatType: type
+    ) -> None:
+        ## Checking against hard-coded test case
+        # fmt: off
+        mat = Mat4( 1.0,  2.0,  3.0,  4.0,
+                    5.0,  6.0,  7.0,  8.0,
+                    9.0, 10.0, 11.0, 12.0,
+                   13.0, 14.0, 15.0, 16.0)
+        factor = 1.5
+        expected_scaled = Mat4( 1.5,  3.0,  4.5,  6.0,
+                                7.5,  9.0, 10.5, 12.0,
+                               13.5, 15.0, 16.5, 18.0,
+                               19.5, 21.0, 22.5, 24.0)
+        # fmt: on
+
+        # Checking __mul__
+        scaled = mat * factor
+        assert type(scaled) is Mat4
+        assert scaled == expected_scaled
+
+        # Checking __rmul__
+        scaled = factor * mat
+        assert type(scaled) == Mat4
+        assert scaled == expected_scaled
+
+        # Checking against some randomly sampled matrices
+        for _ in range(NUM_RANDOM_SAMPLES):
+            np_mat = np.random.randn(4, 4).astype(FloatType)
+            factor = np.random.randn()
+            mat = Mat4(np_mat)
+
+            # Checking __mul__
+            scaled = mat * factor
+            np_scaled = np_mat * factor
+            assert mat4_all_close(scaled, np_scaled)
+
+            # Checking __rmul__
+            scaled = factor * mat
+            np_scaled = factor * np_mat
+            assert mat4_all_close(scaled, np_scaled)
+
+    def test_matrix_vector_product(
+        self, Mat4: Matrix4Cls, Vec4: Vector4Cls, FloatType: type
+    ) -> None:
+        # Checking against hard-coded test case
+        # fmt: off
+        mat = Mat4( 1.0,  2.0,  3.0,  4.0,
+                    5.0,  6.0,  7.0,  8.0,
+                    9.0, 10.0, 11.0, 12.0,
+                   13.0, 14.0, 15.0, 16.0)
+        # fmt: on
+        vec = Vec4(1.0, 2.0, 3.0, 4.0)
+        prod = mat * vec
+        expected_prod = Vec4(30.0, 70.0, 110.0, 150.0)
+        assert type(prod) is Vec4
+        assert prod == expected_prod
+
+        # Checking against some randomly sampled matrices
+        for _ in range(NUM_RANDOM_SAMPLES):
+            np_mat = np.random.randn(4, 4).astype(FloatType)
+            np_vec = np.random.randn(4, 1).astype(FloatType)
+            mat = Mat4(np_mat)
+            vec = Vec4(np_vec)
+
+            np_prod = np_mat @ vec
+            prod = mat * vec
+            assert vec2_all_close(prod, np_prod)
+
+    def test_matrix_matrix_product(
+        self, Mat4: Matrix4Cls, Vec4: Vector4Cls, FloatType: type
+    ) -> None:
+        # Checking against hard-coded test case
+        # fmt: off
+        mat_a = Mat4( 1.0,  2.0,  3.0,  4.0,
+                      5.0,  6.0,  7.0,  8.0,
+                      9.0, 10.0, 11.0, 12.0,
+                     13.0, 14.0, 15.0, 16.0)
+        mat_b = Mat4( 2.0,  3.0,  5.0,  7.0,
+                     11.0, 13.0, 17.0, 19.0,
+                     23.0, 29.0, 31.0, 37.0,
+                     41.0, 43.0, 47.0, 53.0)
+        expected_c = Mat4( 257.,  288.,  320.,  368,
+                           565.,  640.,  720.,  832,
+                           873.,  992., 1120., 1296,
+                          1181., 1344., 1520., 1760)
+        mat_c = mat_a * mat_b
+        # fmt: on
+        assert type(mat_c) == Mat4
+        assert mat_c == expected_c
+
+        # Checking against some randomly sampled matrices
+        for _ in range(NUM_RANDOM_SAMPLES):
+            np_mat_a = np.random.randn(4, 4).astype(FloatType)
+            np_mat_b = np.random.randn(4, 4).astype(FloatType)
+            mat_a, mat_b = Mat4(np_mat_a), Mat4(np_mat_b)
+
+            mat_c = mat_a * mat_b
+            expected_c = np_mat_a @ np_mat_b
+            assert mat4_all_close(mat_c, expected_c)

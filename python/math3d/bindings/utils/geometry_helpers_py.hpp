70dbcd8a18618373d539d082bc273886d98b9144
gregor <wpumacay@gmail.com>
2023-10-02 11:00:56 -0500

    (python): adds bindings for Plane type

--- python/math3d/bindings/utils/geometry_helpers_py.hpp
+++ python/math3d/bindings/utils/geometry_helpers_py.hpp
@@ -45,4 +45,55 @@ auto bindings_utils_line(py::module& m, const char* class_name) -> void {
         });
 }
 
+template <typename T>
+using SFINAE_PLANE_BINDINGS =
+    typename std::enable_if<IsScalar<T>::value>::type*;
+
+template <typename T, SFINAE_PLANE_BINDINGS<T> = nullptr>
+// NOLINTNEXTLINE
+auto bindings_utils_plane(py::module& m, const char* class_name) -> void {
+    using Plane = ::math::Plane<T>;
+    using Vec3 = ::math::Vector3<T>;
+    py::class_<Plane>(m, class_name)
+        .def(py::init<>())
+        .def(py::init<Vec3, Vec3>())
+        .def(py::init([](const py::array_t<T>& np_point,
+                         const py::array_t<T>& np_normal) -> Plane {
+            return Plane(::math::nparray_to_vec3<T>(np_point),
+                         ::math::nparray_to_vec3<T>(np_normal));
+        }))
+        .def_readwrite("point", &Plane::point)
+        .def_readwrite("normal", &Plane::normal)
+        .def("signedDistanceTo",
+             [](const Plane& self, const Vec3& point) -> T {
+                 return self.signedDistanceTo(point);
+             })
+        .def("signedDistanceTo",
+             [](const Plane& self, const py::array_t<T>& np_point) -> T {
+                 return self.signedDistanceTo(
+                     ::math::nparray_to_vec3<T>(np_point));
+             })
+        .def("distanceTo",
+             [](const Plane& self, const Vec3& point) -> T {
+                 return self.distanceTo(point);
+             })
+        .def("distanceTo",
+             [](const Plane& self, const py::array_t<T>& np_point) -> T {
+                 return self.distanceTo(::math::nparray_to_vec3<T>(np_point));
+             })
+        .def("project",
+             [](const Plane& self, const Vec3& point) -> Vec3 {
+                 return self.project(point);
+             })
+        .def("project",
+             [](const Plane& self,
+                const py::array_t<T>& np_point) -> py::array_t<T> {
+                 return ::math::vec3_to_nparray<T>(
+                     self.project(::math::nparray_to_vec3<T>(np_point)));
+             })
+        .def("__repr__", [](const Plane& self) -> py::str {
+            return py::str(self.toString());
+        });
+}
+
 }  // namespace math
